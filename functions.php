afacfa42c0394d723ed558f376688cbbfcc8b40c
Mandi Rocket <mandi018@gmail.com>
2015-05-10 14:05:06 -0700

    Changed red hovers to blue, styled table press, attempt at bringing back the header graphic

--- functions.php
+++ functions.php
@@ -3,9 +3,9 @@
 include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.1.2' );
+define( 'CHILD_THEME_NAME', 'Smart Thermostat Guide Theme' );
+define( 'CHILD_THEME_URL', 'http://www.smartthermostatguide.com' );
+define( 'CHILD_THEME_VERSION', '1.0.0' );
 
 //* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
@@ -23,6 +23,38 @@ add_theme_support( 'genesis-responsive-viewport' );
 
 //* Add support for custom background
 add_theme_support( 'custom-background' );
+
+// Add support for custom header
+add_theme_support( 'genesis-custom-header', array(
+	'width' => 688, //1152 original
+	'height' => 166 //120 original
+) );
+
+//Add a clickable image as the header
+/*
+function inject_clickable_header(){ ?>
+<div id="title-area">
+<a href="http://smartthermostatguide.com/"><img src="http://smartthermostatguide.com/wp-content/uploads/2014/12/stg_banner_2014.png"/></a>
+</div>
+<?php }
+add_action('genesis_header','inject_clickable_header');
+*/
+
+/* Top pick thermostat link for current month and current year - added early 2015 */
+function clickable_header() {
+    ?>
+    <div id="titleAndDescription"><h1><a href="<?php echo get_bloginfo ( 'url' ); ?>"><?php echo get_bloginfo ( 'name' ); ?></a></h1>
+    <p><?php echo get_bloginfo ( 'description' ); ?></p></div>
+    <span id="topPickBanner">
+    Top smart thermostat pick for <?php echo date('F Y');?>: <b>Ecobee3 Wi-Fi Thermostat</b> 
+    (<a href="http://smartthermostatguide.com/ecobee3-smart-thermostat-review/">Review</a> 
+    | <a href="http://www.amazon.com/gp/product/B00NXRYUDA/ref=as_li_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=B00NXRYUDA&linkCode=as2&tag=stg14-20&linkId=3O7KCHAFFKOZDD7U">Shop</a>)</span>
+
+    <!-- <a href="http://smartthermostatguide.com/"><img src="http://smartthermostatguide.com/wp-content/uploads/2014/12/stg_banner_2014.png"></a>-->
+    <?php
+}
+
+add_action('genesis_site_title','clickable_header' );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
@@ -43,6 +75,7 @@ add_theme_support( 'genesis-footer-widgets', 3 );
  * @link http://www.sridharkatakam.com/use-inline-logo-instead-background-image-genesis/
  *
 ************************************/
+
 add_filter( 'genesis_seo_title', 'custom_header_inline_logo', 10, 3 );
 function custom_header_inline_logo( $title, $inside, $wrap ) {
  
@@ -64,7 +97,7 @@ function custom_header_inline_logo( $title, $inside, $wrap ) {
 }
 
 // Remove the site description
-remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+//remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
 
 // Enqueue scripts and styles
 add_action( 'wp_enqueue_scripts', 'custom_scripts_styles_mobile_responsive' );
@@ -72,8 +105,11 @@ function custom_scripts_styles_mobile_responsive() {
 
 	wp_enqueue_script( 'responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
 	wp_enqueue_style( 'dashicons' );
-
 }
+
+add_action('genesis_site_title','clickable_header' );
+remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
 
 // Customize the previous page link
 add_filter ( 'genesis_prev_link_text' , 'sp_previous_page_link' );
@@ -103,3 +139,15 @@ function be_remove_genesis_page_templates( $page_templates ) {
 }
 add_filter( 'theme_page_templates', 'be_remove_genesis_page_templates' );
 
+/*Use excerpts in category view */
+add_action( 'genesis_before', 'child_conditional_actions' );
+function child_conditional_actions() {
+    if( is_category() ) {
+        //put your actions here
+        remove_action( 'genesis_post_content', 'genesis_do_post_content' );
+		remove_action( 'genesis_before_post_content', 'genesis_post_info' );
+        /*remove_action( 'genesis_post_content', 'genesis_do_post_image' );*/
+        add_action( 'genesis_post_content', 'the_excerpt' );
+ 
+    }
+}
